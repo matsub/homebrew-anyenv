d333b935f000fb81080bff5784b78db39ea02da6
matsub <matsub.rk@gmail.com>
2016-12-25 00:38:16 +0900

    fix version: use specific version

--- anyenv.rb
+++ anyenv.rb
@@ -1,8 +1,10 @@
 class Anyenv < Formula
     desc "[anyenv] all in one for **env"
     homepage "https://github.com/riywo/anyenv"
-    head "https://github.com/riywo/anyenv.git"
+    url "https://github.com/riywo/anyenv/archive/3cb8ad1b0dfd89ed5a53fcc9e076b371f6baabfc.zip"
+    sha256 "5bfa5785cdfcf5f6131cd22e1e8011ec41519b00e5e1c586c8bd00e45a054df1"
     version "3cb8ad1"
+    head "https://github.com/riywo/anyenv.git"
 
     def install
         inreplace "libexec/anyenv", %(ANYENV_ROOT="${HOME}/.anyenv"), %(ANYENV_ROOT="#{prefix}")
